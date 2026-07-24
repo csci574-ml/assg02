260794216d52ae496fa2f8ee417aeec98b620070
Derek Harter <derek@harter.pro>
2026-07-24 21:08:54 +0000

    Fix PROJECT_ROOT location
    Moved location of the `assg/utils.py` file one subdirectory lower,
    so need to fix the init of the project root path here.
    
    TODO: Not sure if this works when we install this as a module, e.g. will the location
    of the directory be somwhere other than the project directory, making this
    discovery of the project root not work?  Need to test.

--- src/assg/utils.py
+++ src/assg/utils.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-# determine root directory, this assumes that the the assg_utils.py
-# file is in a child directory 1 level below the project root
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
+# determine root directory, this assumes that the the assg/utils.py
+# file is in a child directory 2 levels below the project root
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 print(PROJECT_ROOT)
