99bdfa0b81854310e461134a2568a5afd03c61a8
Derek Harter <derek@harter.pro>
2026-07-24 21:09:43 +0000

    Remove print statement from utils.py
    Had in a print statement doing some debugging, but don't want it there
    normally.  Remove it.

--- src/assg/utils.py
+++ src/assg/utils.py
@@ -3,4 +3,3 @@ from pathlib import Path
 # determine root directory, this assumes that the the assg/utils.py
 # file is in a child directory 2 levels below the project root
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
-print(PROJECT_ROOT)
